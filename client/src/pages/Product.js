723486ad48b9f02d11ce0c0c9051bf83e182d3ef
ridwan57 <ridwananik57@gmail.com>
2021-03-07 04:39:00 +0600

    lesson124

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { getProduct } from '../functions/product'
+import SingleProduct from '../components/cards/SingleProduct'
 
 const Product = ({ match: { params } }) => {
     const [product, setProduct] = useState({})
@@ -12,10 +13,15 @@ const Product = ({ match: { params } }) => {
         getProduct(params.slug).then(res => setProduct(res.data))
     }
     return (
-        <div>
-            <h1>{params.slug}</h1>
-            {JSON.stringify(product)}
-            <h1>product</h1>
+        <div className='container-fluid'>
+            <div className='row pt-4 h-75' >
+                <SingleProduct product={product} />
+
+            </div>
+            <div className='row '>
+                <div>Related Product</div>
+
+            </div>
         </div>
     )
 }
