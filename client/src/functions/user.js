df8fbbfbe9d5429e3f6eba0913cbc471924ca073
ridwan57 <ridwananik57@gmail.com>
2021-03-09 07:32:41 +0600

    lesson183

--- client/src/functions/user.js
+++ client/src/functions/user.js
@@ -35,7 +35,7 @@ export const emptyUserCart = async (authtoken) => {
 
 export const saveUserAddress = async (authtoken, address) => {
     return await axios.post(
-        `${process.env.REACT_APP_API}/user/cart`,
+        `${process.env.REACT_APP_API}/user/address`,
         { address },
         {
             headers: {
