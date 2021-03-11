c522e34414369c3976d2d69f5016199f96eb952f
ridwan57 <ridwananik57@gmail.com>
2021-03-11 22:01:10 +0600

    package added in a root folder

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { readdirSync } = require("fs");
+const path = require('path')
 require("dotenv").config();
 
 // app
@@ -28,6 +29,14 @@ app.use(cors());
 
 // routes middleware
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+
+//serve
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build/index.html'))
+  })
+}
 
 // port
 const port = process.env.PORT || 8000;
