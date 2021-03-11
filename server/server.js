5db5917391bd72f8d531bf921ef6dce3d91486a4
ridwan57 <ridwananik57@gmail.com>
2021-03-12 00:00:53 +0600

    random

--- server/server.js
+++ server/server.js
@@ -30,13 +30,13 @@ app.use(cors());
 // routes middleware
 readdirSync("./server/routes").map((r) => app.use("/api", require("./routes/" + r)));
 
-//serve
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
-  })
-}
+// //serve
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('/client/build'))
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
+//   })
+// }
 
 // port
 const port = process.env.PORT || 8000;
