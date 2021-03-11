c8f315e3b7e7f669c4dd91be13b2ef61f7a4a4c8
ridwan57 <ridwananik57@gmail.com>
2021-03-12 01:35:29 +0600

    fgsdfg

--- server/server.js
+++ server/server.js
@@ -29,11 +29,11 @@ app.use(cors());
 
 // routes middleware
 readdirSync("./server/routes").map((r) => app.use("/api", require("./routes/" + r)));
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "../client", "build")))
 
 // //serve
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
 });
 
 // port
